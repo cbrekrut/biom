438f51d95eab77a40b6ea166e52f381dd30d17ae
WazowskiMike <wazowskipuffs@yandex.ru>
2024-07-21 15:15:09 +0300

    finish

--- website/views.py
+++ website/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+import telebot
+TELEGRAM_BOT_TOKEN ='7212846432:AAFmFxH5sK0Y2AGc-HWQR18fpUyZIHfNgb0'
+TELEGRAM_CHAT_ID = '-4284612896'
+bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
 def index(request):
+    if request.method == "POST":
+        email = request.POST.get('Email')
+        phone = request.POST.get('phone')
+        message = f"Новый заказ!\nEmail: {email}\nТелефон: {phone}\n"
+        bot.send_message(TELEGRAM_CHAT_ID, message)
     return render(request,'index.html')
